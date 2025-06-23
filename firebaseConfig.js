b21fc75b4846739fe124b0153a531282b0cb3072
genesisfitnessclub <genesisfitnessclub.blr@gmail.com>
2025-06-23 20:10:00 +0530

    Add files via upload

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,1 +1,17 @@
-// Paste your Firebase config here
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAXwxJPqUaAbKmW_miLM5uoiJYdv7OffGU",
+  authDomain: "genesis-fitness-club.firebaseapp.com",
+  projectId: "genesis-fitness-club",
+  storageBucket: "genesis-fitness-club.firebasestorage.app",
+  messagingSenderId: "79182170114",
+  appId: "1:79182170114:web:6a4091b53d9d69baf57f1b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
